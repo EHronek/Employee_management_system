c9bd2e74c9d55f5f5c3b0d6fbabbd4dbb0f9401a
EHronek <emmanuelhronek@gmail.com>
2025-03-20 12:31:29 +0300

    done with most endpoints minus tests

--- api/v1/views/payrolls.py
+++ api/v1/views/payrolls.py
@@ -1,56 +1,65 @@
 #!/usr/bin/python3
+"""Defines endpoint for payroll"""
 from flask import Flask, jsonify, request, abort
 from models import storage
 from models.payroll import Payroll
 from api.v1.views import app_views
 
+
 @app_views.route('/payrolls', methods=['GET'], strict_slashes=False)
 def get_payrolls():
     """Retrieve all payroll records"""
     payrolls = [payroll.to_dict() for payroll in storage.all(Payroll).values()]
-    return jsonify(payrolls)
+    if not payrolls:
+        return jsonify({"error": "payrolls not found"}), 404
+    return jsonify(payrolls), 200
+
 
 @app_views.route('/payrolls/<payroll_id>', methods=['GET'], strict_slashes=False)
 def get_payroll(payroll_id):
     """Retrieve a specific payroll record"""
     payroll = storage.get(Payroll, payroll_id)
     if not payroll:
-        abort(404)
-    return jsonify(payroll.to_dict())
+        return jsonify({"error": "payroll not found"}), 404
+    return jsonify(payroll.to_dict()), 200
+
 
 @app_views.route('/payrolls', methods=['POST'], strict_slashes=False)
 def create_payroll():
     """Create a new payroll record"""
     if not request.json:
-        abort(400, "Not a JSON")
+        jsonify({"error": "Not a JSON"})
     
     required_fields = ["employee_id", "basic_salary", "deductions", "net_pay", "payment_status"]
     for field in required_fields:
         if field not in request.json:
-            abort(400, f"Missing {field}")
+            jsonify({"error": f"Missing {field}"}), 400
     
     data = request.get_json()
     payroll = Payroll(**data)
     payroll.save()
     return jsonify(payroll.to_dict()), 201
 
+
 @app_views.route('/payrolls/<payroll_id>', methods=['PUT'], strict_slashes=False)
 def update_payroll(payroll_id):
     """Update an existing payroll record"""
     payroll = storage.get(Payroll, payroll_id)
     if not payroll:
-        abort(404)
+        jsonify({"error": "payroll not exists"}), 404
     
     if not request.json:
-        abort(400, "Not a JSON")
+        jsonify({"Not a JSON"}), 400
     
     ignored_keys = ["id", "employee_id", "created_at", "updated_at"]
     for key, value in request.json.items():
         if key not in ignored_keys:
-            setattr(payroll, key, value)
+            if hasattr(payroll, key):
+                setattr(payroll, key, value)
     
     payroll.save()
     return jsonify(payroll.to_dict())
+
 
 @app_views.route('/payrolls/<payroll_id>', methods=['DELETE'], strict_slashes=False)
 def delete_payroll(payroll_id):
